71a22021b0d7ec127aa1cead7fbf5beb7fbdbe80
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2025-10-05 01:52:48 +0500

    Updated exam modules

--- models/Score.js
+++ models/Score.js
@@ -3,9 +3,13 @@ import { Schema, model } from "mongoose";
 const scoreSchema = new Schema(
   {
     student: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    class: { type: Schema.Types.ObjectId, ref: "Class", required: true },
+    subject: { type: Schema.Types.ObjectId, ref: "Subject", required: true },
+    campus: { type: Schema.Types.ObjectId, ref: "Campus", required: true },
+
     exam: { type: Schema.Types.ObjectId, ref: "Exam", required: true },
     isPresent: { type: Boolean, default: true },
-    marksObtained: { type: Number, required: true },
+    marksObtained: { type: Number, min: 0, required: true },
 
     // You can calculate Percentage from marks
     remarks: { type: String }, // Optional
