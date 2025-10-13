8c22bd95529c82a4586c7855b6c51e0510075445
Sheheryaar Ansar <sheryaarlong@gmail.com>
2025-10-13 23:57:29 +0500

    Replace rollNumber as an Number into String

--- validators/studentAttendanceValidator.js
+++ validators/studentAttendanceValidator.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const studentAssignmentSchema = Joi.object({
     classId: Joi.string().required(),
     records: Joi.array().items(Joi.object({
-        rollNumber: Joi.number().required,
+        rollNumber: Joi.string().required(),
         status: Joi.string().valid('present', 'absent', 'leave').required()
     }))
 })
